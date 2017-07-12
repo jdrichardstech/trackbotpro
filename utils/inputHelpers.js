3ece7f8ae89cfd4dd49e91fb98c0e088ee46168e
jdrichardstech <jdrichardstech@gmail.com>
2017-07-12 02:29:33 -0400

    refactor helper functions

--- utils/inputHelpers.js
+++ utils/inputHelpers.js
@@ -1,44 +1,40 @@
 
-//creates array of objects for inputs in attachments
-function loopInputNumbers(num){
-  let inputObjectsArray = []
-  for(let i=1;i<num+1;i++){
-		let optionsObject = {"text" : JSON.stringify(i), "value": i}
- 		inputObjectsArray.push(optionsObject)
-  }
-  return inputObjectsArray
-}
-
-//creates array of formatted minutes
-function loopMinutes(num){
-  let inputObjectsArray = []
-
-  for(let i=1;i<num+1;i++){
-    let optionsObject={}
-    if(i<10){
-      optionsObject = {"text" : ":0"+ i.toString(), "value": i}
-      inputObjectsArray.push(optionsObject)
-    } else {
-      	optionsObject = {"text" : ":"+i.toString(), "value": i}
-				inputObjectsArray.push(optionsObject)
-    }
-
-  }
-  return inputObjectsArray
-}
-
-//creates array of objects for distance input in attachments
-function loopDistanceInputNumbers(num){
-	let inputObjectsArray = []
-	for(let i=5;i<num+1;i+=5){
-		let optionsObject = {"text" : JSON.stringify(i), "value": i}
-		inputObjectsArray.push(optionsObject)
-	}
-	return inputObjectsArray
-}
-
 module.exports = {
-	loopInputNumbers: loopInputNumbers,
-	loopDistanceInputNumbers: loopDistanceInputNumbers,
-	loopMinutes: loopMinutes
+	//creates array of objects for inputs in attachments
+	loopInputNumbers: (num) => {
+	  let inputObjectsArray = []
+	  for(let i=1;i<num+1;i++){
+			let optionsObject = {"text" : JSON.stringify(i), "value": i}
+	 		inputObjectsArray.push(optionsObject)
+	  }
+	  return inputObjectsArray
+	},
+
+	//creates array of formatted minutes
+	 loopMinutes: (num) => {
+	  let inputObjectsArray = []
+
+	  for(let i=1;i<num+1;i++){
+	    let optionsObject={}
+	    if(i<10){
+	      optionsObject = {"text" : ":0"+ i.toString(), "value": i}
+	      inputObjectsArray.push(optionsObject)
+	    } else {
+	      	optionsObject = {"text" : ":"+i.toString(), "value": i}
+					inputObjectsArray.push(optionsObject)
+	    }
+
+	  }
+	  return inputObjectsArray
+	},
+
+	//creates array of objects for distance input in attachments
+	loopDistanceInputNumbers: (num) => {
+		let inputObjectsArray = []
+		for(let i=5;i<num+1;i+=5){
+			let optionsObject = {"text" : JSON.stringify(i), "value": i}
+			inputObjectsArray.push(optionsObject)
+		}
+		return inputObjectsArray
+	}
 }
