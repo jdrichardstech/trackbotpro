3d7d216e311678ac7c4533b43e31bff25b0c2637
jdrichardstech <jdrichardstech@gmail.com>
2017-07-14 19:30:35 -0400

    rollback

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,7 @@ const router = express.Router()
 const urlencodedParser = bodyParser.urlencoded({extended:true})
 const Run = require('../models/Run')
 const helpers = require('../utils/inputHelpers');
-const slash = require('../utils/slashCommandHelpers')
-const response = require('../utils/responseHelpers')
 require('dotenv').config()
-// let mainObj = {}
 
 router.get('/', (req, res, next) => {
 	res.status(200).sendFile('/public/index.html')
@@ -43,12 +40,167 @@ router.get('/auth/redirect', (req, res)=>{
 //formats and posts all of the inputs for trackbot
 router.post('/trackbot', function(req, res, next){
 	console.log('reqbody: '+ JSON.stringify(req.body))
-	let reqBody = req.body
-	let responseURL = reqBody.response_url
+	var reqBody = req.body
+	var responseURL = reqBody.response_url
 	var userName = req.body.user_name
 	let editedUserName = userName.charAt(0).toUpperCase()+userName.substr(1)
 
-	slash.command(reqBody, editedUserName, responseURL)
+	switch(req.body.text){
+		case 'view':
+			var botPayload = {
+				"response_type": 'ephemeral',
+				"text": "*"+editedUserName +"\'s Log*\n\n*Date*\t*ExerciseType*\t*Distance*\t*Time*"
+			}
+			sendMessageToSlackResponseURL(responseURL, botPayload)
+			break;
+		case 'help':
+			var botPayload = {
+			"response_type": 'ephemeral',
+			"text": "Hi *" + editedUserName + "*! What would you like to do? \n\nType:\n `/trackbot add` to add a new exercise or \n`/trackbot view` to view your exercises or \n`/trackbot leaderboard` to view who is in the lead this week."
+			}
+			sendMessageToSlackResponseURL(responseURL, botPayload)
+			break;
+		case 'add':
+			var botPayload = {
+				"response_type": 'ephemeral',
+				"text": "Hi *" + editedUserName + "*!",
+				"attachments": [
+					 {
+						 "text":"Choose your Exercise Type:",
+						 "mrkdwn": true,
+						 "fallback": "You are unable to choose an exercise",
+						 "callback_id": "exerciseType",
+						 "color": "#3AA3E3",
+						 "attachment_type": "default",
+						 "actions": [
+							 {
+								 "name": "run",
+								 "text": "Run",
+								 "type": "button",
+								 "value": "run"
+							 },
+							 {
+								 "name": "walk",
+								 "text": "Walk",
+								 "type": "button",
+								 "value": "walk"
+							 },
+							 {
+								 "name": "bike",
+								 "text": "Bike",
+								 "type": "button",
+								 "value": "bike"
+							 }
+						 ]
+					 },
+					 {
+						 "text":"What was your distance?:",
+						 "mrkdwn": true,
+						 "fallback": "You are unable to choose an distance",
+						 "callback_id": "exerciseDistance",
+						 "color": "#ff3333",
+						 "attachment_type": "default",
+						 "actions":[
+							 {
+								"name": 'distanceNumber',
+								"text": 'Distance',
+								"type": 'select',
+								"value": 'distanceNumber',
+								"style": 'primary',
+								"options": helpers.loopDistanceInputNumbers(45)
+							 },
+							 {
+								"name": 'distanceFormat',
+								"text": 'Miles or Kilometers',
+								"type": 'select',
+								"value": 'distanceFormat',
+								"style": 'primary',
+								"options": [
+									{
+										"text": "mile(s)",
+										"value": "miles"
+									},
+									{
+										"text": "kilometer(s)",
+										"value": "kilometers"
+									}
+								]
+							 }
+						 ]
+					 },
+					 {
+						 "text":"How long was your exercise?",
+						 "mrkdwn": true,
+						 "fallback": "You are unable to choose an time",
+						 "callback_id": "exerciseTime",
+						 "color": "#00cc44",
+						 "attachment_type": "default",
+						 "actions":[
+							 {
+								"name": 'hour',
+								"text": 'Hours',
+								"type": 'select',
+								"value": 'hour',
+								"style": 'primary',
+								"options": helpers.loopInputNumbers(20)
+								},
+								{
+									"name": 'minutes',
+									"text": 'Minutes',
+									"type": 'select',
+									"value": 'minutes',
+									"style": 'primary',
+									"options": helpers.loopMinutes(59)
+								}
+							]
+						},
+						{
+						 "text":"Submit exercise to your log?",
+						 "mrkdwn": true,
+						 "fallback": "Not done",
+						 "callback_id": "completeExercise",
+						 "color": "#fff",
+						 "attachment_type": "default",
+						 "actions": [
+							 {
+								 "name": "submit",
+								 "text": "Submit",
+								 "type": "button",
+								 "value": "submit",
+								 "confirm": {
+									 "title": "Are you sure?",
+									 "text": "Think about it.",
+									 "ok_text": "Yes",
+									 "dismiss_text": "No"
+									}
+							 },
+							 {
+								 "name": "cancel",
+								 "text": "Cancel",
+								 "type": "button",
+								 "value": "cancel"
+							 }
+							]
+						}
+					]
+				}
+				sendMessageToSlackResponseURL(responseURL, botPayload)
+				break;
+			case 'leaderboard':
+				var botPayload = {
+				"response_type": 'ephemeral',
+				"text": "Hi *" + editedUserName + "*! Here is the leaderboard"
+				}
+				sendMessageToSlackResponseURL(responseURL, botPayload)
+				break;
+			default:
+				var botPayload = {
+				"response_type": 'ephemeral',
+				"text": "Hi *" + editedUserName + "*! What would you like to do? \n\nType:\n `/trackbot add` to add a new exercise or \n`/trackbot view` to view your exercises or \n`/trackbot leaderboard` to view who is in the lead this week."
+				}
+				sendMessageToSlackResponseURL(responseURL, botPayload)
+				break;
+				}
 })
 
 //posts each action and will send to req.body info to sendMessageToSlackResponseURL function
@@ -61,11 +213,7 @@ router.post('/log/actions', urlencodedParser, (req, res) =>{
 	userID,
 	teamID,
 	channelID,
-	clicked,
-	exerciseType,
-	mainObj,
-	userObj
-
+	clicked
 
 	if(token == process.env.VERIFICATION_TOKEN){
 		teamID=payload.team.id
@@ -73,106 +221,69 @@ router.post('/log/actions', urlencodedParser, (req, res) =>{
 		channelID=payload.channel.id
 		actions = payload.actions[0]
 		clicked = actions.name
-		userObj = helpers.mainObj[(teamID+userID)]
-		userKey = teamID + userID
-		userName = payload.user.name
 
+		console.log("payload------TEAM ID:" + teamID + " CHANNEL ID: " + channelID + "----UNIQUEID: " + teamID + userID + "---USERID: " + userID  )
 		//only fires for select_option inputs
 		if(actions.selected_options!=undefined){
 			selectedValue = actions.selected_options[0].value
 		}
-
-		if(payload.callback_id == 'exerciseType'){
-			userObj.exerciseType = clicked
-			return
-		}
-
-		switch(clicked){
-			case 'distanceNumber':
-				userObj.exerciseDistance = selectedValue
-				break;
-			case 'distanceType':
-				userObj.distanceType = selectedValue
-				break;
-			case 'exerciseHours':
-				userObj.exerciseHours = selectedValue
-				break;
-			case 'exerciseMinutes':
-				userObj.exerciseMinutes = selectedValue
-				break;
-			case 'cancel':
-				console.log('USEROBJECT: ' + JSON.stringify(helpers.mainObj))
-				if(helpers.mainObj[userKey]){
-					helpers.mainObj[userKey]={}
-				}
-				console.log('USEROBJECT IS DELETED FROM MAINOBJ: ' + JSON.stringify(helpers.mainObj))
-				break;
-			default:
-				break;
-		}
-
-
-		// CHECK FOR IDS
-		// console.log("payload------TEAM ID:" + teamID + " CHANNEL ID: " + channelID + "----UNIQUEID: " + teamID + userID )
-
-		//STATIC OBJECT FOR ENTERING STATIC DB RECORDS
-		// let runObj = {
-		// 	userID:'K123sk4',
-		// 	teamID:'NKeko3333',
-		// 	exersiseDate: Date.now(),
-		// 	exerciseMinutes: 680,
-		// 	channelID: 'KDK39d9',
-		// 	userName: 'genestd',
-		// 	distanceType: 'miles',
-		// 	exerciseDistance: 12.5,
-		// 	exerciseType: 'run',
-		// 	exerciseHours: 1,
-		// 	timestamp: Date.now()
+		// let runObj ={
+		// 	userKey:'T645HNGB1U645HNGDD',
+		// 	userID:'U645HNGDD',
+		// 	teamID:'T645HNGB1',
+		// 	channelID:'C62L8JPEU',
+		// 	userName:'jdrichardstech',
+		// 	exerciseDate: Date.now(),
+		// 	exerciseType: 'bike',
+		// 	exerciseDistance:30,
+		// 	distanceType:'kilometers',
+		// 	exerciseHours:2,
+		// 	exerciseMinutes:15
 		// }
-
+			//cretes a new run record in the database
 			if(clicked == 'submit'){
-
-				let keys = {
-					userID,
-					teamID,
-					channelID,
-					userKey,
-					userName
-				}
-
-				//ADD THE REST OF THE ID'S TO THE MAIN OBJECT
-				for(let k in keys) userObj[k] = keys[k]
-				console.log("SUBMITTED USEROBJECT: " + JSON.stringify(userObj))
-				//ADD EXERCISE OBJECT TO DATABASE
-				// createRun(userObj)
-				// //DELETE CURRENT USER OBJECT IN MAIN OBJECT
-				if(helpers.mainObj[userKey]){
-					delete helpers.mainObj[userKey]
-				}
-				console.log('USEROBJECT IS DELETED FROM MAINOBJ: ' + JSON.stringify(helpers.mainObj))
-				return
+				console.log('SUMBITTED: ' + JSON.stringify(runObj))
+				// createRun(runObj)
 			}
-		} else {
+		} else{
 			console.log("VERIFICATION_TOKEN ERROR")
-			return
 		}
 
-		//MESSAGE TO SEND BACK TO SLACK INTERFACE
-			// let message = {
-			// 	"text": "{" + userID + "}" + payload.user.name + "--- CALLBACK_ID: " + payload.callback_id + "--- TYPE: "+actions.type + "--- CLICKED(aka name): " + clicked + " ---VALUE: "+selectedValue,
-			// 	"replace_original": false
-			// }
-			// console.log('SENDMESSAGE OBJECT: ' + JSON.stringify(message))
-		//FUNCTION SENDS MESSAGE REMBEMBER IMPORT TAG
-		// sendMessageToSlackResponseURL(payload.response_url, message)
+	// test message to send to sendMessageToSlackResponseURL function to show when a button or input is clicked
+		let message = {
+			"text": "{" + userID + "}" + payload.user.name + "--- CALLBACK_ID: " + payload.callback_id + "--- TYPE: "+actions.type + "--- CLICKED(aka name): " + clicked + " ---VALUE: "+selectedValue,
+			"replace_original": false
+		}
+		console.log(message)
+	//thfunction below can be used for validation of inputs
+	// sendMessageToSlackResponseURL(payload.response_url, message)
 })
 
-//CREATE NEW RUN INSTANCE AND SEND TO DATABASE----MOVE TO HELPER FILE
+//function that creates a new run object and saves it to mongo database
 function createRun(obj){
 	const newRun = new Run(obj)
 	newRun.save()
-	console.log('Added a new exercise record into collection');
+	console.log('Added a new slack user todo list document into collection');
  	return newRun;
+}
+
+//posts messages back to slack.
+function sendMessageToSlackResponseURL(responseURL, JSONmessage){
+  var postOptions = {
+      uri: responseURL,
+      method: 'POST',
+      headers: {
+          'Content-type': 'application/json'
+      },
+      json: JSONmessage
+  }
+  request(postOptions, (error, response, body) => {
+      if (error){
+          console.log("ERROR: sendMessageToSlackResponseURL error" + error)
+      }else{
+				// console.log("RESPONSE: " + JSON.stringify(response))
+			}
+  })
 }
 
 module.exports = router
